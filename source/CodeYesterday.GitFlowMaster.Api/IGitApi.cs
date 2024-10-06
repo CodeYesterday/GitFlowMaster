3f617665f803375502093d0c2b818f44d06c0631
Reinhard <osre@aarpg.net>
2024-10-06 10:05:39 +0200

    [GFM-3] Create GIT API interface

---
+++ source/CodeYesterday.GitFlowMaster.Api/IGitApi.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeYesterday.GitFlowMaster.Api;
+
+[PublicAPI]
+public interface IGitApi
+{
+    Task FetchAsync(bool prune = false, bool pruneTags = false);
+
+    Task PullAsync();
+
+    Task PushAsync();
+
+    Task GetBranchListAsync();
+}
